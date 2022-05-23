b0c6d5163859ba7bc97a6db669cdce0a8d0124e5
김 수정 <kim@gim-ui-iMac.local>
2022-05-23 16:22:36 +0900

    react_state_prac_making-nemo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Nemo from "./nemo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 3
+    };
+
+  }
+
+  componentDidMount(){
+
+  }
+
+  addNemo = () => {
+    this.setState({count: this.state.count + 1});
+  }
+  removeNemo = () => {
+    if(this.state.count > 0){
+      this.setState({count: this.state.count - 1})
+    }else{
+      window.alert("네모가 없어요!");
+    }
+    
+  }
+
+  render(){
+    // const nemo_count = Array.from({length:this.state.count}, (v,i)=> i);
+
+    // console.log(this.state)
+
+    return(
+      <div className="App">
+        <Nemo/>
+        {/* {nemo_count.map((n,i)=> {
+          return <div 
+          key={i}
+          style={{
+            width: "150px",
+            height: "150px",
+            backgroundColor: "#ddd",
+            margin: "10px"
+            }}>
+            nemo
+          </div>
+        })}
+        <div>
+          <button onClick={this.addNemo}>하나 추가</button>
+          <button onClick={this.removeNemo}>하나 빼기</button>
+        </div> */}
+      </div>
+    )
+  }
 }
-
 export default App;
